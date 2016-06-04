712ffbf4bc8535d575dbd3091b2c3d49b91743cd
Jonah Graham <jonah@kichwacoders.com>
2016-06-04 21:22:21 +0100

    markers on print stack trace

--- Exercises/Automate/ex2_markers.py
+++ Exercises/Automate/ex2_markers.py
@@ -1,16 +1,16 @@
-# 1. use the resources module 
- 
+# 1. use the resources module
+
 IMarker = org.eclipse.core.resources.IMarker
- 
+
 # 2. TODO:use findFiles method to get all *.java files in the workspace
 # 3. use 'for' loop to iterate over files, (using ifile as your variable name)
 
 # 4. Uncomment these lines to get file location, open file and iterate through lines
-# 
+#
 #    file_name = str(ifile.getLocation())
 #    with open(file_name) as f:
 #        for line_no, line in enumerate(f, start=1):
-#            if "TODO" in line:
+#            if "printStackTrace" in line:
 
 # 5. Create a marker(Hint method is on ifile and use type IMarker.TASK
 
--- Solutions/Automate Solutions/markers.py
+++ Solutions/Automate Solutions/markers.py
@@ -1,13 +1,13 @@
 loadModule('/System/Resources')
- 
+
 # Access the markers class directly
 IMarker = org.eclipse.core.resources.IMarker
- 
+
 for ifile in findFiles("*.java"):
     file_name = str(ifile.getLocation())
     with open(file_name) as f:
         for line_no, line in enumerate(f, start=1):
-            if "TODO" in line:
+            if "printStackTrace" in line:
                 marker = ifile.createMarker(IMarker.TASK)
                 marker.setAttribute(IMarker.TRANSIENT, True)
                 marker.setAttribute(IMarker.LINE_NUMBER, line_no)
