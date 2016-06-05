52392293a6b6090cd0da5c617b464dcb16a50467
Jonah Graham <jonah@kichwacoders.com>
2016-06-05 01:52:18 +0100

    Full listviewer UI with buttons that add and remove from the model

--- Solutions/Integrate Solutions/listviewer.py
+++ Solutions/Integrate Solutions/listviewer.py
@@ -1,34 +1,81 @@
-loadModule("/System/Platform")
 loadModule("/System/UI")
+loadModule("/System/Platform")
+
+# import logging
+# logger = logging.getLogger("py4j")
+# logger.setLevel(logging.DEBUG)
+# logger.addHandler(logging.StreamHandler())
 
 ArrayContentProvider = org.eclipse.jface.viewers.ArrayContentProvider
 LabelProvider = org.eclipse.jface.viewers.LabelProvider
 ListViewer = org.eclipse.jface.viewers.ListViewer
 SWT = org.eclipse.swt.SWT
-FillLayout = org.eclipse.swt.layout.FillLayout;
-Display = org.eclipse.swt.widgets.Display;
-Shell = org.eclipse.swt.widgets.Shell;
+FillLayout = org.eclipse.swt.layout.FillLayout
+RowLayout = org.eclipse.swt.layout.RowLayout
+Display = org.eclipse.swt.widgets.Display
+Shell = org.eclipse.swt.widgets.Shell
+Button = org.eclipse.swt.widgets.Button
 
-class MyListViewer(object):
-    def __init__(self, shell):
-        v = ListViewer(shell, SWT.H_SCROLL | SWT.V_SCROLL)
-        v.setLabelProvider(LabelProvider())
-        v.setContentProvider(ArrayContentProvider.getInstance())
-        v.setInput(self.createModel())
-        
-    def createModel(self):
-        elements = ["Item %s" % i for i in range(10)]
-        return elements
-
-        
 def uimain():
     display = Display.getCurrent()
-    shell = Shell(display)
-    shell.setLayout(FillLayout())
-    MyListViewer(shell)
-    shell.open()
+    dialog = Shell(display, SWT.DIALOG_TRIM | SWT.MAX | SWT.RESIZE)
+    dialog.setLayout(RowLayout())
+
+    listViewer = ListViewer(dialog)
+    listViewer.setLabelProvider(LabelProvider())
+    listViewer.setContentProvider(ArrayContentProvider.getInstance())
+
+    # create initial entries
+    initialCount = 10
+    elements = java.util.ArrayList(["Item {}".format(i) for i in range(initialCount)])
+    listViewer.setInput(elements)
+
+    add = Button(dialog, SWT.PUSH)
+    add.setText("Add Data")
+    class AddDataSelectionListener(object):
+        def __init__(self, start):
+            self.count = start
+
+        def widgetSelected(self, e):
+            elements.append("Item {}".format(self.count))
+            self.count += 1
+            listViewer.refresh()
+
+        def equals(self, other):
+            return self is other
+        def hashCode(self):
+            return id(self)
+        def widgetDefaultSelected(self, e):
+            pass
+        class Java:
+            implements = ['org.eclipse.swt.events.SelectionListener']
+    add.addSelectionListener(AddDataSelectionListener(initialCount))
+
+    remove = Button(dialog, SWT.PUSH)
+    remove.setText("Remove Selected Data")
+    class RemoveDataSelectionListener(object):
+        def widgetSelected(self, e):
+            for remove in listViewer.getSelection().toArray():
+                elements.remove(remove)
+            listViewer.refresh()
+
+        def equals(self, other):
+            return self is other
+        def hashCode(self):
+            return id(self)
+        def widgetDefaultSelected(self, e):
+            pass
+        class Java:
+            implements = ['org.eclipse.swt.events.SelectionListener']
+    remove.addSelectionListener(RemoveDataSelectionListener())
+
+    dialog.pack()
+    dialog.open()
+    while dialog is not None and not dialog.isDisposed():
+        if not display.readAndDispatch():
+            display.sleep()
+    if not display.isDisposed():
+        display.update()
+
 
 executeUI("uimain()")
-# while True:
-#     print("Still running")
-#     time.sleep(1)
