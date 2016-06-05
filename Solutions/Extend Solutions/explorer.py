f248125656581de5a050d4b593421a25a0330ed0
Jonah Graham <jonah@kichwacoders.com>
2016-06-05 01:51:44 +0100

    Fix newlines

--- Solutions/Extend Solutions/explorer.py
+++ Solutions/Extend Solutions/explorer.py
@@ -11,13 +11,13 @@ is_instance_of = py4j.java_gateway.is_instance_of
 selection = getSelection()
 if is_instance_of(gateway, selection, org.eclipse.jface.viewers.IStructuredSelection):
 	selection = selection.getFirstElement()
-	
+
 if not is_instance_of(gateway, selection, org.eclipse.core.resources.IResource):
 	selection = adapt(selection, org.eclipse.core.resources.IResource)
 
 if is_instance_of(gateway, selection, org.eclipse.core.resources.IFile):
 	selection = selection.getParent()
-	
+
 if is_instance_of(gateway, selection, org.eclipse.core.resources.IContainer):
 	subprocess.call(["explorer.exe", selection.getLocation().toFile().toString()])
-	
+
