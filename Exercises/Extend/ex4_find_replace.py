7330ff741c34aad5b3841f5ff926a2bbc0d6bd0a
Tracy Miranda <tracy@kichwacoders.com>
2016-06-05 10:54:33 +0100

    Add in exercise 4 find replace

---
+++ Exercises/Extend/ex4_find_replace.py
@@ -0,0 +1,16 @@
+# 0. Use magic headers to create a popup that is enabled for java.lang.Object.
+
+# 1. load the UI module
+
+def find_replace(find, replace):
+    print(find, replace)
+    # 2. get the active editor (Hint: use method from UI module)
+    # 3. from editor get the document provider then get the document (passing in editor input)
+    # 4. from editor get the selection provider then the selection
+    # 5. from selection get text
+    # 6. perform a replace on the text
+    # 7. if text has changed
+    # 8. in document, replace the selection with the new text
+
+# 9. uncomment this line to run the methond
+#executeUI("find_replace('private', 'public')")
