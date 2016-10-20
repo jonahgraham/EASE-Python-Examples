de3f2f6fcd07eec8830fed767633915e52556d4c
Tracy Miranda <tracy@kichwacoders.com>
2016-10-20 13:36:54 +0100

    Update dawn plotting and provide solution for plotting in dawn

--- Utilities/dawnplotting.py
+++ Utilities/dawnplotting.py
@@ -1,5 +1,8 @@
+# This file adds scisoftpy to the Python Interpreter path
+# The remoteport must match the port specified on the Preferences>DAWN>Analysis RPC & RMI
+# The AnalysisRPC port is the one required
 import sys
 sys.path.append('''C:\data\Projects\NOBUGS\plugins\uk.ac.diamond.scisoft.python_1.3.0''')
 import scisoftpy as dnp  # @UnresolvedImport
-dnp.plot.setremoteport(51046)
+dnp.plot.setremoteport(55614)
 
