f2ae714fb1d7969287b440fd096991e60939f1fc
Jonah Graham <jonah@kichwacoders.com>
2017-05-17 09:48:05 +0100

    Revert master to be non-DAWN dependent version
    The DAWN dependent examples are in dawn_examples branch

--- Solutions/Integrate Solutions/charting_dawn.py
+++
@@ -1,9 +0,0 @@
-include('workspace://Utilities/dawnplotting.py')
-import scisoftpy as dnp
-
-
-t = dnp.arange(0.0, 2.0, 0.01)
-s = dnp.sin(2 * dnp.pi * t)
-dnp.plot.line(t, s)
-ps = dnp.plot.getPlottingSystem()
-ps.setTitle("EclipseCon France Demo")
